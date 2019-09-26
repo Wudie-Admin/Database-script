9d464c95777038adb599cf5d0eac970d7784c342
Wudalat <'wgetahun@tech-res.com>
2019-09-25 22:16:27 -0400

    Currency Table Base Version

---
+++ Staging/Staging/curreny.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[curreny]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [currency_code] CHAR(10) NOT NULL, 
+    [Currency_name] NVARCHAR(50) NULL, 
+    [Currency_Description] NVARCHAR(50) NULL
+)
