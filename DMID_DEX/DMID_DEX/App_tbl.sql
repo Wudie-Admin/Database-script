43204c2bb4184fcdf5468def3d3e2439c862ca47
Wudalat <'wgetahun@tech-res.com>
2019-10-03 14:33:58 -0400

    Dex test table

---
+++ DMID_DEX/DMID_DEX/App_tbl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[App_tbl]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Fname] NCHAR(10) NULL, 
+    [Lname] NCHAR(10) NULL, 
+    [Phone] NCHAR(10) NULL, 
+    [email] NCHAR(10) NULL, 
+    [Address] NCHAR(10) NULL
+)
